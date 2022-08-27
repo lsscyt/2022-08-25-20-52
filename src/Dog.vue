00e13c2b9efe930acc3283e9b09b4e46b753cc5f
lss <3104649492@qq.com>
2022-08-27 14:13:02 +0800

    2022/08/27

---
+++ src/Dog.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app"><Dog></Dog><Dog></Dog><Dog></Dog></div>
+</template>
+
+<script>
+import Dog from "@/components/Dog.vue";
+export default {
+  components: { Dog },
+};
+</script>
+
+<style></style>
---
+++ src/components/Dog.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src="@/assets/mv1.jpg" alt="" />
+    <h1>美女</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    
+    };
+  },
+};
+</script>
+
+<style>
+img {
+  width: 320px;
+}
+</style>
